d8d31e76dc65ea614314f6256c2069352b0c051c
Vincent Tiono <b11901123@ntu.edu.tw>
2025-04-24 20:51:32 +0800

    now train state_sequence

--- pretrain/cfg_option_new_vae.py
+++ pretrain/cfg_option_new_vae.py
@@ -342,7 +342,7 @@ config = {
     'cover_all_branches_in_demos': True,            # If True, make sure to cover all branches in randomly generated program in ExecEnv1
     'final_reward_scale': False,
 
-    'behavior_representation': 'action_sequence',    # 'state_sequence', 'action_sequence'
+    'behavior_representation': 'state_sequence',    # 'state_sequence', 'action_sequence'
     'fuse_s_0': True,                               # 'True', fuse s_0 with a_h embedding after RNN. 'False': concatenate s_0 with a_h then RNN
     'loss': {
         'latent_loss_coef': 1.0,                    # coefficient of latent loss (beta) in VAE during SL training
@@ -350,7 +350,7 @@ config = {
         'enabled_losses': {
             'z_rec': True,
             'b_z_rec': True,
-            'contrastive_loss': 'contrastive',      # 'contrastive', 'clip'
+            'contrastive_loss': 'clip',      # 'contrastive', 'clip'
             'latent': True,
             'z_condition': True,
             'b_z_condition': True,
